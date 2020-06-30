44421b8e2a3d29d652e524436946487d7e04b152
AmesingFlank <ldfrank533@outlook.com>
2020-06-30 02:19:33 +0100

    build

---
+++ pages/home/home-app/build/precache-manifest.0804b65d18d156f9753224480d8180d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700f7df9b063ea7a5487ddef1bef1e7f",
+    "url": "/pages/home/home-app/build/index.html"
+  },
+  {
+    "revision": "8bd0b7f3b7d8c590b0f8",
+    "url": "/pages/home/home-app/build/static/css/main.229145e5.chunk.css"
+  },
+  {
+    "revision": "cb592218124cc9998a4e",
+    "url": "/pages/home/home-app/build/static/js/2.0f12c022.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pages/home/home-app/build/static/js/2.0f12c022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bd0b7f3b7d8c590b0f8",
+    "url": "/pages/home/home-app/build/static/js/main.2bf0421f.chunk.js"
+  },
+  {
+    "revision": "6c9b688efabf761292f3",
+    "url": "/pages/home/home-app/build/static/js/runtime-main.ea12f4e8.js"
+  }
+]);
